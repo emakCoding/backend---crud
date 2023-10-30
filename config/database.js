7b122f04803172b95d7ca23b5b02d6100a4b3ab5
Tari <ummu.fasa1886@gmail.com>
2023-10-30 20:12:43 +0700

    konfigurasi  database.js

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@
 
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize("crud_db", "root", "", {
-  host: "localhost",
+const db = new Sequelize(process.env.PLANETSCALE_DB, process.env.PLANETSCALE_DB_USERNAME, process.env.PLANETSCALE_DB_PASSWORD, {
+  host: process.env.PLANETSCALE_DB_HOST,
   dialect: "mysql",
 });
 
